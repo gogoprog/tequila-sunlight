579cc7f2544ff437982e318fbe9ca76864ac2b3a
gogoprog <gogoprog@gmail.com>
2012-12-03 17:21:26 +0100

    Fixed alignment

--- src/daemon/light_manager.cpp
+++ src/daemon/light_manager.cpp
@@ -8,52 +8,52 @@ LightManager::LightManager()
 
 void LightManager::addCommand(const string & command)
 {
-	boost::mutex::scoped_lock
-		lock(commandMutex);
+    boost::mutex::scoped_lock
+        lock(commandMutex);
 
-	commands.push_back(command);
+    commands.push_back(command);
 }
 
 void LightManager::update(const int ticks)
 {
-	vector<string>
-		commands_copy;
-	vector<string>::iterator
-		iter;
+    vector<string>
+        commands_copy;
+    vector<string>::iterator
+        iter;
 
-	{
-		boost::mutex::scoped_lock
-			lock(commandMutex);
+    {
+        boost::mutex::scoped_lock
+            lock(commandMutex);
 
-		commands_copy = commands;
+        commands_copy = commands;
 
-		commands.clear();
-	}
+        commands.clear();
+    }
 
-	for(iter=commands_copy.begin(); iter != commands_copy.end(); ++iter)
-	{
-		string
-			command;
+    for(iter=commands_copy.begin(); iter != commands_copy.end(); ++iter)
+    {
+        string
+            command;
 
-		command = *iter;
+        command = *iter;
 
-		cout << "Command: " << command << endl;
-	}
+        cout << "Command: " << command << endl;
+    }
 }
 
 // Private
 
 void LightManager::initializeComPort()
 {
-	for(int i=0; i<4; ++i)
-	{
-		cout << "TequilaSunlight: opening com port " << i << endl;
-		comPort.open(i);
+    for(int i=0; i<4; ++i)
+    {
+        cout << "TequilaSunlight: opening com port " << i << endl;
+        comPort.open(i);
 
-		if(comPort.isOpen())
-		{
-			cout << "TequilaSunlight: com port " << i << " opened!" << endl;
-			return;
-		}
-	}
+        if(comPort.isOpen())
+        {
+            cout << "TequilaSunlight: com port " << i << " opened!" << endl;
+            return;
+        }
+    }
 }
--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -8,63 +8,63 @@
 using namespace std;
 
 LightManager
-	lightManager;
+    lightManager;
 
 static void *callback(enum mg_event event, mg_connection *conn) 
 {
     const mg_request_info 
-		* request_info = mg_get_request_info(conn);
+        * request_info = mg_get_request_info(conn);
 
-	if (event == MG_NEW_REQUEST)
-	{
-		string
-			command(request_info->uri);
+    if (event == MG_NEW_REQUEST)
+    {
+        string
+            command(request_info->uri);
 
-		lightManager.addCommand(command);
+        lightManager.addCommand(command);
 
-		return ( void * )1;
-	}
-	else
-	{
-		return NULL;
-	}
+        return ( void * )1;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 void httpJob()
 {
-	mg_context 
-		* context;
-	const char 
-		* options[] = { "listening_ports", "8080", NULL };
+    mg_context 
+        * context;
+    const char 
+        * options[] = { "listening_ports", "8080", NULL };
 
-	context = mg_start(&callback, NULL, options);
+    context = mg_start(&callback, NULL, options);
 
-	while(1)
-	{
-		boost::this_thread::yield();
-	}
+    while(1)
+    {
+        boost::this_thread::yield();
+    }
 }
 
 void lightJob()
 {
-	while(1)
-	{
-		lightManager.update(50);
-		boost::this_thread::sleep(boost::posix_time::milliseconds(50));
-		boost::this_thread::yield();
-	}
+    while(1)
+    {
+        lightManager.update(50);
+        boost::this_thread::sleep(boost::posix_time::milliseconds(50));
+        boost::this_thread::yield();
+    }
 }
 
 int main()
 {
-	boost::thread
-		http_job_thread(httpJob),
-		light_job_thread(lightJob);
+    boost::thread
+        http_job_thread(httpJob),
+        light_job_thread(lightJob);
 
-	cout << "TequilaSunlight starting..." << endl;
+    cout << "TequilaSunlight starting..." << endl;
 
-	boost::this_thread::yield();
+    boost::this_thread::yield();
 
-	http_job_thread.join();
-	light_job_thread.join();
+    http_job_thread.join();
+    light_job_thread.join();
 }
